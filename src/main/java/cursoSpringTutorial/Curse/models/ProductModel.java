d883469c63978a17581de7412223e8b3f0758cf4
Victor Emanuel <victor.emanuel@sgnsistemas.com.br>
2024-09-02 14:34:28 -0300

    correção do problema de instancia do Model com dados

--- src/main/java/cursoSpringTutorial/Curse/models/ProductModel.java
+++ src/main/java/cursoSpringTutorial/Curse/models/ProductModel.java
@@ -1,14 +1,22 @@
 package cursoSpringTutorial.Curse.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "TB_PRODUCTS")
-public class ProductModel  implements Serializable {
+public class ProductModel implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
