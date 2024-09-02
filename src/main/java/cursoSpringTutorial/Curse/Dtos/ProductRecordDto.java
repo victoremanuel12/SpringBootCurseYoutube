085dedd15d452a5d9c59484621c129ead4d4eead
Victor Emanuel <victor.emanuel@sgnsistemas.com.br>
2024-09-02 14:17:24 -0300

    criação do controlador mas com problemas de fazer o parse do dto para o model

--- src/main/java/cursoSpringTutorial/Curse/Dtos/ProductRecordDto.java
+++ src/main/java/cursoSpringTutorial/Curse/Dtos/ProductRecordDto.java
@@ -1,5 +1,10 @@
 package cursoSpringTutorial.Curse.Dtos;
 
-public record ProductRecordDto() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
 
 }
--- src/main/java/cursoSpringTutorial/Curse/controllers/ProductController.java
+++ src/main/java/cursoSpringTutorial/Curse/controllers/ProductController.java
@@ -1,13 +1,26 @@
 package cursoSpringTutorial.Curse.controllers;
 
+import cursoSpringTutorial.Curse.Dtos.ProductRecordDto;
+import cursoSpringTutorial.Curse.models.ProductModel;
 import cursoSpringTutorial.Curse.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
     @Autowired
-    private ProductRepository productService;
+    private ProductRepository productRepository;
 
-
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 }
