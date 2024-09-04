e3671d36a2ab46bf29a13906d7de71666203db2d
Victor Emanuel <victoremanuel12@hotmail.com>
2024-09-03 23:22:01 -0300

    finalização do curso basico

---
+++ src/main/java/cursoSpringTutorial/Curse/Dtos/ProductRequestRecordDto.java
@@ -0,0 +1,11 @@
+package cursoSpringTutorial.Curse.Dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRequestRecordDto(@NotBlank(message = "O nome do produto não pode estar em branco") String name,
+                                      @NotNull BigDecimal value) {
+
+}
--- src/main/java/cursoSpringTutorial/Curse/controllers/ProductController.java
+++ src/main/java/cursoSpringTutorial/Curse/controllers/ProductController.java
@@ -1,6 +1,6 @@
 package cursoSpringTutorial.Curse.controllers;
 
-import cursoSpringTutorial.Curse.Dtos.ProductRecordDto;
+import cursoSpringTutorial.Curse.Dtos.ProductRequestRecordDto;
 import cursoSpringTutorial.Curse.models.ProductModel;
 import cursoSpringTutorial.Curse.repositories.ProductRepository;
 import jakarta.validation.Valid;
@@ -14,13 +14,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
     @PostMapping("/products")
-    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRequestRecordDto productRecordDto) {
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
@@ -28,7 +31,14 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts() {
-        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.findAll());
+        List<ProductModel> productList = productRepository.findAll();
+        if (!productList.isEmpty()) {
+            for (ProductModel product : productList) {
+                UUID id = product.getUuid();
+                product.add(linkTo(methodOn(ProductController.class).getById(id)).withSelfRel());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(productList);
     }
 
     @GetMapping("/products/{id}")
@@ -37,11 +47,12 @@ public class ProductController {
         if (product.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
+        product.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withSelfRel());
         return ResponseEntity.status(HttpStatus.OK).body(product.get());
     }
 
     @PutMapping("/products/{id}")
-    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id, @RequestBody @Valid ProductRecordDto productRecordDto) {
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id, @RequestBody @Valid ProductRequestRecordDto productRecordDto) {
         Optional<ProductModel> product = productRepository.findById(id);
         if (product.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
